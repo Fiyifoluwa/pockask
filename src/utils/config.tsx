af644897ef965e1a3902f72b9ad210abd723183f
Fiyifoluwa <fiyifoluwa@outlook.com>
2024-12-10 20:15:03 +0100

    utility functions

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -1,55 +1,10 @@
-import React from 'react';
-import Box from '../components/Box';
-import Text from '../components/Text';
-import {ToastConfigParams} from 'react-native-toast-message';
-
-interface ToastProps {
-  text: string;
-}
-
 interface TaxRate {
   rate: number;
   threshold?: number;
 }
 
-export const toastConfig = {
-  successToast: ({text1, props}: Partial<ToastConfigParams<ToastProps>>) => (
-    <Box px="xxl" py="sml" bg="malachite100" mt="ll" borderRadius="m">
-      {text1 && <Text color="white">{text1}</Text>}
-      <Text color="malachite800" textAlign="center" variant="toastText">
-        {props?.text ?? ''}
-      </Text>
-    </Box>
-  ),
-  infoToast: ({text1, props}: Partial<ToastConfigParams<ToastProps>>) => (
-    <Box px="xxl" py="sml" bg="ochre100" mt="ll" borderRadius="m">
-      {text1 && <Text color="white">{text1}</Text>}
-      <Text color="ochre800" textAlign="center" variant="toastText">
-        {props?.text ?? ''}
-      </Text>
-    </Box>
-  ),
-  dangerToast: ({text1, props}: Partial<ToastConfigParams<ToastProps>>) => (
-    <Box px="xxl" py="sml" bg="cardinal50" mt="ll" borderRadius="m">
-      {text1 && <Text color="white">{text1}</Text>}
-      <Text color="red" textAlign="center" variant="toastText">
-        {props?.text ?? ''}
-      </Text>
-    </Box>
-  ),
-};
-
-const TAX_RATES: TaxRate[] = [
+export const TAX_RATES: TaxRate[] = [
   {rate: 0.05, threshold: 100},
   {rate: 0.08, threshold: 500},
   {rate: 0.1},
 ];
-
-export const calculateTax = (price: number, quantity: number): number => {
-  const totalPrice = price * quantity;
-  const applicableRate =
-    TAX_RATES.find(({threshold}) => !threshold || totalPrice <= threshold) ||
-    TAX_RATES[TAX_RATES.length - 1];
-
-  return totalPrice * applicableRate.rate;
-};
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,5 @@
+import {TAX_RATES} from './config';
+
 /**
  * Calculates the total credit or debit amount of transactions within a date range.
  *
@@ -8,9 +10,9 @@
  */
 export const formatAmount = (
   amount: string | number,
-  currency: string = 'NGN',
+  currency: string = 'USD',
 ) => {
-  const formatter = new Intl.NumberFormat('en-NG', {
+  const formatter = new Intl.NumberFormat('en-US', {
     currency,
     minimumFractionDigits: 2,
     style: 'currency',
@@ -19,17 +21,26 @@ export const formatAmount = (
   const roundedAmount = parseFloat(Number(amount).toFixed(2));
 
   if (!isNaN(roundedAmount) && roundedAmount !== 0) {
-    const fullAmount = formatter.format(roundedAmount / 100);
+    const fullAmount = formatter.format(roundedAmount);
     const parts = fullAmount.split('.');
-    const currencySymbol = parts[0][0]; // "₦"
+    const currencySymbol = parts[0][0]; // "$"
     const wholeNumberPart = parts[0].slice(1); // "1,234"
     const fractionalPart = parts[1]; // "56"
 
     return `${currencySymbol}${wholeNumberPart}.${fractionalPart}`;
   }
-  return '₦0.00';
+  return '$0.00';
 };
 
 export const capitalizeFirstLetter = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export const calculateTax = (price: number, quantity: number): number => {
+  const totalPrice = price * quantity;
+  const applicableRate =
+    TAX_RATES.find(({threshold}) => !threshold || totalPrice <= threshold) ||
+    TAX_RATES[TAX_RATES.length - 1];
+
+  return totalPrice * applicableRate.rate;
 };
